aca42c92320badc1f19d619ce3e700841a361dc0
Zack White <zrwhite2993@gmail.com>
2019-12-17 22:31:10 -0600

    made changes to handlebars

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var app = express();
 var exphbs = require('express-handlebars')
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
 
 var routes = require('./burger/controllers/burgers_controller.js');
