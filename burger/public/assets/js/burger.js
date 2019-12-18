bf8c4d4d83724dd35a7477c7aab50129a0ffc834
Zack White <zrwhite2993@gmail.com>
2019-12-18 12:00:37 -0600

    Changed file directories

---
+++ burger/public/assets/js/burger.js
@@ -0,0 +1,51 @@
+$(function() {
+  $(".change-hunger").on("click", function(event) {
+    var id = $(this).data("id");
+    var hunger = $(this).data("hunger");
+
+    var hungerState = {
+      hungry: hunger
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: hungerState
+    }).then(
+      function() {
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burg").val().trim(),
+      hungry: $("[name=devoured]:checked").val().trim()
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log(newBurger)
+        location.reload();
+      }
+    );
+  });
+
+  $('delete-burger').on('click', function(event) {
+    let id = $(this).data('id');
+
+    $.ajax('/api/burgers/' + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        location.reload();
+      }
+    );
+  });
+});
